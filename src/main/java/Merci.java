95b00291dfbff46cf09bf7d6cdf1ed7576cf577a
ryanr <ryanrongchangtan@gmail.com>
2026-01-29 14:18:47 +0800

    Added Level 3: Mark as done

--- src/main/java/Merci.java
+++ src/main/java/Merci.java
@@ -6,25 +6,37 @@ public class Merci {
         System.out.println("What can I do for you?\n");
 
         Scanner sc = new Scanner(System.in);
-
-        String input;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
-        while (true) {
-            input = sc.nextLine();
+        while (true){
+            String input = sc.nextLine();
 
-            if (input.equalsIgnoreCase("list")){
-                for (int i = 0; i < taskCount; i++){
-                    System.out.println((i + 1) + ".  " + tasks[i]);
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + tasks[i].toString());
                 }
-            } else if (input.equalsIgnoreCase("bye")){
+            }
+            else if (input.equalsIgnoreCase("bye")){
                 System.out.println("bye! see you again soon!");
                 break;
-            } else {
-                tasks[taskCount] = input;
+            } else if (input.toLowerCase().startsWith("mark ")){
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                tasks[index].markAsDone();
+                System.out.println("Nice! I've marked this as done: ");
+                System.out.println(tasks[index].toString());
+            } else if (input.toLowerCase().startsWith("unmark ")){
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(tasks[index].toString());
+            }
+            else {
+                tasks[taskCount] = new Task(input);
                 taskCount++;
-                System.out.println("added: " + tasks[taskCount - 1]);
+                System.out.println("Added: " + tasks[taskCount - 1].getDescription());
             }
         }
     }
 }
+
