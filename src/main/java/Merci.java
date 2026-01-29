129510d032c7d603dfddc4df6bb3f94db2468ffd
ryanr <ryanrongchangtan@gmail.com>
2026-01-29 12:35:24 +0800

    Added Level-2:Add, List

--- src/main/java/Merci.java
+++ src/main/java/Merci.java
@@ -3,19 +3,28 @@ import java.util.Scanner;
 public class Merci {
     public static void main(String[] args) {
         System.out.println("Hello! I am Merci!!");
-        System.out.println("What can I do for you?");
-        System.out.println();
+        System.out.println("What can I do for you?\n");
 
         Scanner sc = new Scanner(System.in);
-        String echo;
 
-        while (true){
-            echo = sc.nextLine();
-            if (echo.equalsIgnoreCase("bye")) {
-                System.out.println("bye! hope to see you again soon!!");
+        String input;
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        while (true) {
+            input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("list")){
+                for (int i = 0; i < taskCount; i++){
+                    System.out.println((i + 1) + ".  " + tasks[i]);
+                }
+            }
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println("bye! see you again soon!");
                 break;
             }
-            System.out.println(echo);
+            tasks[taskCount] = input;
+            taskCount++;
+            System.out.println("added: " + tasks[taskCount - 1]);
         }
     }
 }
